fe5af675e3bd5f4f43e83b6a75b8822f1232ba5e
Ohm-Tiwari <otiwari24@gmail.com>
2024-04-03 12:46:21 -0400

    Made changes

--- src/main/java/edu/iu/habahram/GumballMachine/service/GumballService.java
+++ src/main/java/edu/iu/habahram/GumballMachine/service/GumballService.java
@@ -1,9 +1,6 @@
 package edu.iu.habahram.GumballMachine.service;
 
-import edu.iu.habahram.GumballMachine.model.GumballMachine;
-import edu.iu.habahram.GumballMachine.model.GumballMachineRecord;
-import edu.iu.habahram.GumballMachine.model.IGumballMachine;
-import edu.iu.habahram.GumballMachine.model.TransitionResult;
+import edu.iu.habahram.GumballMachine.model.*;
 import edu.iu.habahram.GumballMachine.repository.IGumballRepository;
 import org.springframework.stereotype.Service;
 
@@ -59,34 +56,31 @@ public class GumballService implements IGumballService{
     }
 
     @Override
-    public TransitionResult Quarter(String id) throws IOException{
+    public TransitionResult performAction(String id, Action action) throws IOException {
         GumballMachineRecord record = gumballRepository.findById(id);
         IGumballMachine machine = new GumballMachine(record.getId(), record.getState(), record.getCount());
-
-        if(){
-            TransitionResult result = machine.insertQuarter();
-
+        TransitionResult result = null;
+        switch (action) {
+            case INSERT_QUARTER:
+                result = machine.insertQuarter();
+                break;
+            case EJECT_QUARTER:
+                result = machine.ejectQuarter();
+                break;
+            case TURN_CRANK:
+                result = machine.turnCrank();
+                break;
         }
-        else if(){
-            TransitionResult result = machine.ejectQuarter();
-
-        }
-        else if(){
-            TransitionResult result = machine.turnCrank();
-
-        }
-
-
-        if(result.succeeded()) {
+        if (result != null && result.succeeded()) {
             record.setState(result.stateAfter());
             record.setCount(result.countAfter());
             save(record);
         }
         return result;
-
     }
 
-    
+
+
 
     @Override
     public List<GumballMachineRecord> findAll() throws IOException {
