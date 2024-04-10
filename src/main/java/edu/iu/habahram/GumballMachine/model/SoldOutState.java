da462899acf4f76d60fb77d95b98c1be2e9cbbce
Ohm-Tiwari <otiwari24@gmail.com>
2024-04-10 09:06:13 -0400

    Made changes

---
+++ src/main/java/edu/iu/habahram/GumballMachine/model/SoldOutState.java
@@ -0,0 +1,39 @@
+package edu.iu.habahram.GumballMachine.model;
+
+public class SoldOutState implements IState{
+    IGumballMachine gumballMachine;
+    public SoldOutState(IGumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+    @Override
+    public TransitionResult insertQuarter() {
+        gumballMachine.changeTheStateTo(GumballMachineState.HAS_QUARTER);
+        String message = "You inserted a quarter";
+        boolean succeeded = true;
+        int count = gumballMachine.getCount();
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), count);
+    }
+    @Override
+    public TransitionResult ejectQuarter() {
+        String message = "You ejected a quarter";
+        boolean succeeded = true;
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), gumballMachine.getCount());
+    }
+    @Override
+    public TransitionResult turnCrank() {
+        String message = "You turned, but there's no candy";
+        boolean succeeded = false;
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), gumballMachine.getCount());
+    }
+    @Override
+    public TransitionResult dispense() {
+        String message = "Candy sold out";
+        boolean succeeded = false;
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), gumballMachine.getCount());
+
+    }
+    @Override
+    public String getTheName() {
+        return GumballMachineState.NO_QUARTER.name();
+    }
+}
---
+++ src/main/java/edu/iu/habahram/GumballMachine/model/SoldState.java
@@ -0,0 +1,39 @@
+package edu.iu.habahram.GumballMachine.model;
+
+public class SoldState implements IState{
+    IGumballMachine gumballMachine;
+    public SoldState(IGumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+    @Override
+    public TransitionResult insertQuarter() {
+        gumballMachine.changeTheStateTo(GumballMachineState.HAS_QUARTER);
+        String message = "You inserted a quarter";
+        boolean succeeded = true;
+        int count = gumballMachine.getCount();
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), count);
+    }
+    @Override
+    public TransitionResult ejectQuarter() {
+        String message = "You ejected a quarter";
+        boolean succeeded = true;
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), gumballMachine.getCount());
+    }
+    @Override
+    public TransitionResult turnCrank() {
+        String message = "You turned the crank";
+        boolean succeeded = true;
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), gumballMachine.getCount());
+    }
+    @Override
+    public TransitionResult dispense() {
+        String message = "Candy dispensed";
+        boolean succeeded = true;
+        return new TransitionResult(succeeded, message, gumballMachine.getTheStateName(), gumballMachine.getCount());
+
+    }
+    @Override
+    public String getTheName() {
+        return GumballMachineState.NO_QUARTER.name();
+    }
+}
